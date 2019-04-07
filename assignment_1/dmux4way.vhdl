f418f4c3508f1d1a790c07eb495aca0e56faddb4
dsurf <dshultzemail@gmail.com>
2019-04-06 21:18:14 -0400

    added dmux4way and testbench

---
+++ assignment_1/dmux4way.vhdl
@@ -0,0 +1,36 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity dmux4way is
+  port(
+    i0 : in std_logic;
+    sel: in std_logic_vector(1 downto 0);
+    out_4: out std_logic_vector(3 downto 0)
+    );
+end entity;
+
+architecture rtl of dmux4way is
+  component dmux is
+    port(
+      i0 : in std_logic;
+      sel: in std_logic;
+      out0: out std_logic;
+      out1: out std_logic
+      );
+  end component dmux;
+
+  signal s0, s1, out0, out1, out2, out3 : std_logic;
+
+begin
+
+  dmux0 : dmux port map(i0 => i0, sel => sel(1), out0 => s0, out1 => s1);
+  dmux1a: dmux port map(i0 => s0, sel => sel(0), out0 => out3, out1 => out2);
+
+  dmux1b: dmux port map(i0 => s1, sel => sel(0), out0 => out1, out1 => out0);
+
+  out_4(0) <= out0;
+  out_4(1) <= out1;
+  out_4(2) <= out2;
+  out_4(3) <= out3;
+
+end rtl;
---
+++ assignment_1/dmux4way_tb.vhdl
@@ -0,0 +1,61 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity dmux4way_tb is
+end dmux4way_tb;
+
+architecture behav of dmux4way_tb is
+  component dmux4way is
+    port(
+      i0 : in std_logic;
+      sel: in std_logic_vector(1 downto 0);
+      out_4: out std_logic_vector(3 downto 0)
+      );
+  end component dmux4way;
+
+  for dmux0 : dmux4way use entity work.dmux4way;
+
+  signal in0 : std_logic;
+  signal out4: std_logic_vector(3 downto 0);
+  signal sel : std_logic_vector(1 downto 0);
+  
+begin
+
+  dmux0 : dmux4way 
+    port map(
+      i0 => in0,
+      sel => sel,
+      out_4 => out4
+      );
+
+  process
+
+    constant inputs : std_logic_vector(7 downto 0) := "00001111";
+
+    type sel_patterns is array(natural range <>) of std_logic_vector(1 downto 0);
+    constant sel_inputs : sel_patterns :=
+      ("00","01","10","11","00","01","10","11");
+
+    type out_patterns is array(natural range <>) of std_logic_vector(3 downto 0);
+    constant outputs : out_patterns :=
+      ("1000","0100","0010","0001","0000","0000","0000","0000");
+
+  begin
+    for i in inputs'range loop
+
+      in0 <= inputs(i);
+      sel <= sel_inputs(i);
+
+      wait for 1 ns;
+
+      assert out4 = outputs(i)
+        report "incorrect output" severity error;
+
+    end loop;
+
+    assert false report "end of test" severity note;
+
+    wait;
+
+  end process;
+end behav;
